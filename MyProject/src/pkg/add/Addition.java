c591c5c6eca1f93aa9c2a3da8d17f4d0d93895a3
manju-sakthi <sakthimanju098@gmail.com>
2024-12-13 10:40:10 +0530

    code upload

---
+++ MyProject/src/pkg/add/Addition.java
@@ -0,0 +1,14 @@
+package pkg.add;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=10;
+        int b=12;
+        int c=a+b;
+        
+        System.out.println("Sum is "+c);
+	}
+
+}
